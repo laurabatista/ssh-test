791b65ca9e6f4940a944bea1c00b19991efc3e6e
Gomes Batista Laura <Laura.GomesBatista@interdiscount.ch>
2021-01-18 10:51:58 +0100

    updated action

--- create_authorized_keys.py
+++ create_authorized_keys.py
@@ -5,6 +5,5 @@ open('authorized_keys', 'r+').truncate(0)
 for key in user_keys:
     with open("./users/{}".format(key), "r") as file_1, open("authorized_keys", "a") as file_2:
         for line in file_1:
-          print(line)
-          if "ssh-rsa" in line:
-            file_2.write(line)
+            if "ssh-rsa" in line:
+                file_2.write(line)
