8aeb0f34c05c5def4dc3da42e9ce049b7c268fe0
Gomes Batista Laura <Laura.GomesBatista@interdiscount.ch>
2021-01-18 09:54:36 +0100

    added print to script

--- create_authorized_keys.py
+++ create_authorized_keys.py
@@ -5,5 +5,6 @@ open('authorized_keys', 'r+').truncate(0)
 for key in user_keys:
     with open("./users/{}".format(key), "r") as file_1, open("authorized_keys", "a") as file_2:
         for line in file_1:
-            if "ssh-rsa" in line:
-                file_2.write(line)
+          print(line)
+          if "ssh-rsa" in line:
+            file_2.write(line)
